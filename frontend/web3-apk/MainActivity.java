26f40fe9a2f2905bef7a91e4b55dd3bf33959275
Elparadisogonzalo <azehagowa@elparadisogonzalo.com>
2026-04-09 00:56:16 +0100

    approve

---
+++ frontend/web3-apk/MainActivity.java
@@ -0,0 +1,26 @@
+package com.elparadisogonzalo.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://elparadisogonzalo-u8qwl.pyxl.ai/");
+    }
+}
