5c8234f53d22dabbb0b1f7755a083e182e43adec
koagonzalo11 <azehagowa@elparadisogonzalo.com>
2026-04-08 17:40:48 +0000

    approve

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.elparadisogonzalo.app',
+  appName: 'elparadisogonzalo app',
+  webDir: 'build'
+};
+
+export default config;
---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Elparadisogonzalo Web3 Frontend</h1>
+  </React.StrictMode>
+);
